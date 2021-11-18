ecda0d831dbb0c7d8c4d073a3c6b6ef78a766bb4
liangliangche <2631274697@qq.com>
2021-11-18 23:12:25 +0800

    l

--- src/main/java/com/liangliangche/firstmod/Init/ModBlocks.java
+++ src/main/java/com/liangliangche/firstmod/Init/ModBlocks.java
@@ -2,6 +2,9 @@ package com.liangliangche.firstmod.Init;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.liangliangche.firstmod.Main;
+import com.liangliangche.firstmod.Items.ItemBase;
 import com.liangliangche.firstmod.blocks.BlockBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -10,5 +13,8 @@ import net.minecraft.block.material.Material;
 public class ModBlocks {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
-	public static final Block First_Block =new BlockBase("first_block",Material.IRON);
+	public static final Block First_Block =new BlockBase("first_block",Material.IRON,ItemBase.wry333);
+	
+	//If you want to creat a NEW tab of your items, use the code below.
+	//public static final Block First_Block =new BlockBase("first_block",Material.IRON, Main.MOD_TAB1);
 }
